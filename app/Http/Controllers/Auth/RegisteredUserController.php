de2169ecd829544f090abec06820763f0e861303
wesley viana <wesley.vmartins.js@gmail.com>
2023-08-19 15:02:45 -0300

    Updated controller for save super

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,6 +40,7 @@ class RegisteredUserController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'super' => $request->super,
             'password' => Hash::make($request->password),
         ]);
 
