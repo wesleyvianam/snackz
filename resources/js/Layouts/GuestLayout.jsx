d846bd97c18e1885534ad99803fbec01bc067ce5
wesley viana <wesley.vmartins.js@gmail.com>
2023-08-19 14:16:47 -0300

    created img login page

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,6 +1,7 @@
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import { Link } from '@inertiajs/react';
 import LogoText from '../Components/LogoText';
+import Background from '../../../public/images/auth-img.png';
 
 export default function Guest({ children }) {
     return (
@@ -16,9 +17,9 @@ export default function Guest({ children }) {
                     {children}
                 </div>
             </div>
-            <div className='w-3/4 py-10 pe-10'>
-                <div className="bg-primary rounded-3xl min-h-full flex justify-center items-center text-blue-500 text-3xl">
-                    IMAGE QUE O NIKOLAS VAI FAZER
+            <div className='w-3/4 py-16 pe-16'>
+                <div className="flex justify-center items-center text-blue-500">
+                    <img src={Background} alt="imagem background" className="rounded-3xl min-h-full"/>
                 </div>
             </div>
         </div>
