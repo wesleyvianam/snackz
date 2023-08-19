24d23c3ab6e3fad4666dbb5828fe159301274548
wesley <wesley.vmartins.js@gmail.com>
2023-08-19 11:36:33 -0300

    Update in logo for large

--- resources/js/Components/LogoText.jsx
+++ resources/js/Components/LogoText.jsx
@@ -2,6 +2,6 @@
 
 export default function LogoText({children}) {
   return (
-    <h1 className="text-3xl font-bold text-primary">{children}</h1>
+    <h1 className="text-4xl font-bold text-primary">{children}</h1>
   );
 }
