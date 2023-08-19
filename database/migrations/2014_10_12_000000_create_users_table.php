abb08c8c4db8ec66dbcec549ca9f79de4df540c2
wesley viana <wesley.vmartins.js@gmail.com>
2023-08-19 14:59:46 -0300

    Alter migration user, added super and parent_id

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('super');
+            $table->integer('parent_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
