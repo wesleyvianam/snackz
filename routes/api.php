5903802ff14261102619858698fbddb95455186f
wesley viana <wesley.vmartins.js@gmail.com>
2023-08-20 10:26:25 -0300

    update name of methods

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/teste', [AuthenticationController::class, 'index'])->middleware('auth:sanctum');
 
-Route::post('register', [RegisterController::class, 'store']);
+Route::post('register', [RegisterController::class, 'register']);
 
-Route::post('login', [AuthenticationController::class, 'store']);
+Route::post('employee', [RegisterController::class, 'register']);
+
+Route::post('login', [AuthenticationController::class, 'login']);
