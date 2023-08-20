2e2dcc5506cfbea1ef09158ec0a181ca6fd41536
wesley viana <wesley.vmartins.js@gmail.com>
2023-08-20 14:25:47 -0300

    Created migration snackOptions

---
+++ database/migrations/2023_08_20_151257_create_snack_options_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('snack_options', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+
+            $table->foreignId('snack_item_id')->constrained()->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('snack_options');
+    }
+};
