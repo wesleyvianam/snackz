bd5b898564ffa6843f21cc7d5fab8385eccd1c55
wesley viana <wesley.vmartins.js@gmail.com>
2023-08-20 15:38:16 -0300

    Created Relations

--- app/Models/SnackItems.php
+++ app/Models/SnackItems.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class SnackItems extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'description', 'user_id'];
+
+    public function snackOptions(): HasMany
+    {
+        return $this->hasMany(SnackOptions::class, 'snack_item_id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
