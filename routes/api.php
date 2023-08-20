e958b7453d441d7ae4ca07616140a6c39c92948c
wesley viana <wesley.vmartins.js@gmail.com>
2023-08-20 14:25:00 -0300

    Create group routes and route snack

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\AuthenticationController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Profile\ProfileController;
+use App\Http\Controllers\Snack\SnackItemsController;
 use App\Http\Controllers\Team\TeamController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,11 +19,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::put('profile', [ProfileController::class, 'update'])->middleware('auth:sanctum');
-Route::get('team', [TeamController::class, 'index'])->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::put('profile', [ProfileController::class, 'update']);
+
+    Route::get('team', [TeamController::class, 'index']);
+
+    Route::post('team', [RegisterController::class, 'register']);
+
+    Route::apiResource('/snack', SnackItemsController::class)
+        ->except(['index', 'show', 'create', 'edit']);
+});
 
 Route::post('register', [RegisterController::class, 'register']);
-
-Route::post('employee', [RegisterController::class, 'register']);
 
 Route::post('login', [AuthenticationController::class, 'login']);
