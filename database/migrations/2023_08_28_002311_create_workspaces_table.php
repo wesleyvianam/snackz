85d622d0cceaa9d385124023e4d94331dee77ae2
wesley <wesley.vmartins.js@gmail.com>
2023-08-28 13:21:21 -0300

    change name for nullable

--- database/migrations/2023_08_28_002311_create_workspaces_table.php
+++ database/migrations/2023_08_28_002311_create_workspaces_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('workspaces', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
 
             $table->foreignId('user_id')->constrained();
 
