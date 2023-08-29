2883f36e3257576f9a2ffa689d811968c55662ba
wesley viana <wesley.vmartins.js@gmail.com>
2023-08-29 06:54:24 -0300

    created crud of snack

--- app/Http/Controllers/Snack/SnackController.php
+++ app/Http/Controllers/Snack/SnackController.php
@@ -3,8 +3,40 @@
 namespace App\Http\Controllers\Snack;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Snack;
+use Illuminate\Http\Request;
 
 class SnackController extends Controller
 {
+    public function index(Category $category)
+    {
+        $snack = $category->snacks()->get();
 
+        return response()->json($snack);
+    }
+
+    public function store(Category $category, Request $request)
+    {
+        $snack = Snack::create([
+            "name" => $request->name,
+            "category_id" => $category->id
+        ]);
+
+        return response()->json($snack);
+    }
+
+    public function update(Category $category, Snack $snack, Request $request)
+    {
+        $snack->update([
+            "name" => $request->name
+        ]);
+
+        return response()->json($snack);
+    }
+
+    public function destroy(Category $category, Snack $snack)
+    {
+        return response()->json("Remove code");
+    }
 }
