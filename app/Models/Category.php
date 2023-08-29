858c00c0fa3301bee5e87253e4ac5d468c77cb46
wesley viana <wesley.vmartins.js@gmail.com>
2023-08-29 06:53:18 -0300

    Added relation with snacks

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
@@ -18,5 +19,10 @@ class Category extends Model
     public function workspace(): BelongsTo
     {
         return $this->belongsTo(Workspace::class);
+    }
+
+    public function snacks(): HasMany
+    {
+        return $this->hasMany(Snack::class);
     }
 }
