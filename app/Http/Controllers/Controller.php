109782030fa13513931590b2425216d50cda1630
wesley viana <wesley.vmartins.js@gmail.com>
2023-08-29 06:28:54 -0300

    Created abstract method of workspace

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Workspace;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function getWorkspace()
+    {
+        $workspace = Workspace::where('user_id', Auth::user()->id)->get();
+
+        return $workspace[0];
+    }
 }
