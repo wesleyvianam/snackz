745285579b1a95223999e425d89ac50d139aace5
wesley <wesley.vmartins.js@gmail.com>
2023-08-29 17:25:58 -0300

    .

--- app/Http/Controllers/Order/OrderController.php
+++ app/Http/Controllers/Order/OrderController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers\Order;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
+    public function store(Request $request)
+    {
+        DB::transaction(function () use ($request) {
 
+            $order = [];
+            foreach ($request->snack_id as $snack) {
+                $order[] = Order::create([
+                    'snack_id' => $snack,
+                    'member_id' => $request->member,
+                ]);
+
+                if ($request->recurrent) {
+
+                }
+            }
+
+        });
+    }
 }
