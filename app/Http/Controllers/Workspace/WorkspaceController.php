82e885742c7f80533e4faf28a02f92bdeac5a3b9
wesley viana <wesley.vmartins.js@gmail.com>
2023-08-29 07:01:26 -0300

    Created update and list of workspace

--- app/Http/Controllers/Workspace/WorkspaceController.php
+++ app/Http/Controllers/Workspace/WorkspaceController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers\Workspace;
 
 use App\Http\Controllers\Controller;
+use App\Models\Workspace;
+use Illuminate\Http\Request;
 
 class WorkspaceController extends Controller
 {
+    public function index()
+    {
+        $workspace = $this->getWorkspace();
 
+        return response()->json($workspace);
+    }
+
+    public function update(Workspace $workspace, Request $request)
+    {
+        $workspace->update([
+            "name" => $request->name
+        ]);
+
+        return response()->json($workspace);
+    }
 }
