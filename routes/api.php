a612e24d1d88074fe7636734aacfe076e913bed9
wesley viana <wesley.vmartins.js@gmail.com>
2023-08-29 07:01:07 -0300

    Created routes of Workspace

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Category\CategoryController;
 use App\Http\Controllers\Snack\SnackController;
 use App\Http\Controllers\User\AuthenticationController;
 use App\Http\Controllers\User\RegisterController;
+use App\Http\Controllers\Workspace\WorkspaceController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -13,6 +14,9 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::apiResource('/{category}/snacks', SnackController::class)
         ->except(['view']);
+
+    Route::apiResource('/workspace', WorkspaceController::class)
+        ->except(['view', 'store', 'destroy']);
 });
 
 Route::post('register', [RegisterController::class, 'register']);
