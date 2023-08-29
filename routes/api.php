3ca57e96f68c86b8e00d0a6f582646cb3c1a4f2c
wesley viana <wesley.vmartins.js@gmail.com>
2023-08-29 06:52:44 -0300

    Added routes of snack

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,17 @@
 <?php
 
 use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Snack\SnackController;
 use App\Http\Controllers\User\AuthenticationController;
-use App\Http\Controllers\User\ProfileController;
 use App\Http\Controllers\User\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::middleware('auth:sanctum')->group(function () {
 
-    Route::apiResource('/category', CategoryController::class)
+    Route::apiResource('/categories', CategoryController::class)
+        ->except(['view']);
+
+    Route::apiResource('/{category}/snacks', SnackController::class)
         ->except(['view']);
 });
 
