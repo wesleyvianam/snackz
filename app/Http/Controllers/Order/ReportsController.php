e897ce98b4984a2b2245c2557f33085529adf523
wesley <wesley.vmartins.js@gmail.com>
2023-08-30 12:54:16 -0300

    created report all

---
+++ app/Http/Controllers/Order/ReportsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Order;
+
+use App\Http\Controllers\Controller;
+use App\Models\Member;
+
+class ReportsController extends Controller
+{
+    public function listAll()
+    {
+        $workspace = $this->getWorkspace();
+
+        $orders = Member::where('workspace_id', $workspace->id)
+            ->with('orders.snack')->get();
+        return response()->json($orders);
+    }
+}
