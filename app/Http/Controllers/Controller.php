53a67e907496fa1c4e1420a0855230cd08db7613
wesley <wesley.vmartins.js@gmail.com>
2023-08-30 12:52:31 -0300

    Updated getWorkspace ID for Member

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Member;
 use App\Models\Workspace;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Auth;
+use SebastianBergmann\CodeCoverage\Driver\WriteOperationFailedException;
 
 class Controller extends BaseController
 {
@@ -14,7 +16,10 @@ class Controller extends BaseController
 
     public function getWorkspace()
     {
-        $workspace = Auth::user()->workspace()->get();
+
+        $member = Member::where('user_id', Auth::user()->id)->get();
+
+        $workspace = Workspace::where('id', $member[0]->workspace_id)->get();
 
         return $workspace[0];
     }
