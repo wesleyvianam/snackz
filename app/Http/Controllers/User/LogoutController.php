ad4cd20a2adde658180dc09362f9886b7c26eba4
wesley viana <wesley.vmartins.js@gmail.com>
2023-08-31 06:15:27 -0300

    Created logout

--- app/Http/Controllers/User/LogoutController.php
+++ app/Http/Controllers/User/LogoutController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers\User;
 
-use http\Client\Request;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LogoutController
 {
     public function logout(Request $request)
     {
+        Auth::user()->tokens()->delete();
 
+        return response()->json(['message' => 'To the next.']);
     }
 }
