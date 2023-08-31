e8ddf184af07edfbe5edf6713654f04d664b7f46
wesley viana <wesley.vmartins.js@gmail.com>
2023-08-31 20:57:53 -0300

    created logic of recover password

--- app/Http/Controllers/User/RecoverController.php
+++ app/Http/Controllers/User/RecoverController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Mail\Recover;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
 
 class RecoverController extends Controller
 {
@@ -15,34 +18,86 @@ class RecoverController extends Controller
     {
         $email = $request->email;
 
-        $token = "shdjklfhslkjdfhlskjdhfl";
+        $token = Str::random(60);
 
-        $this->saveToken($email, $token);
+        $result = $this->saveToken($email, $token);
 
-        $send = Mail::to("$email", 'name person')->send(new Recover([
+        $send = Mail::to($email, 'name person')->send(new Recover([
             'fromEmail' => 'Snackz',
             'token' => $token,
+            'email' => $email,
             'message' => 'Use Token para refazer sua senha',
         ]));
 
-        return response()->json($send);
+        return response()->json("Email enviado com successo");
     }
 
-    public function update()
+    public function update(Request $request)
     {
+        $has = $this->verifyToken($request->email, $request->token);
 
+        if (!$has) abort(403, 'Token ou Email Invalidos');
+
+        if ($request->password == $request->confirmpassword) {
+            $user = User::where('email', $request->email)->update([
+                'password' => Hash::make($request->password)
+            ]);
+
+            if ($user) {
+                if ($this->deleteToken($request->email, $request->token)) {
+                    return response()->json('Successfully in update password');
+                }
+            }
+        }
+
+        return response()->json('Error ao tentar salvar nova senha.');
     }
 
-    private function saveToken($email, $token)
+    private function saveToken(string $email, string $token): bool
     {
+        $ItHasPromptForNewPassword = $this->verifyEmail($email);
 
-        $query = DB::table('password_reset_tokens')
+        if ($ItHasPromptForNewPassword) {
+            return DB::table('password_reset_tokens')
+                ->where('email', $email)
+                ->update([
+                    'token' => $token,
+                    'created_at' => now()
+                ]);
+        }
+
+        return DB::table('password_reset_tokens')
             ->insert([
                 'email' => $email,
                 'token' => $token,
-                'created_at' => new \DateTime()
+                'created_at' => now()
             ]);
+    }
 
-        dd($query);
+    private function verifyEmail(string $email): bool
+    {
+        $result = DB::table('password_reset_tokens')
+            ->where('email', '=', $email)
+            ->get();
+
+        return !empty($result[0]);
+    }
+
+    private function verifyToken(string $email, string $token): bool
+    {
+        $result = DB::table('password_reset_tokens')
+            ->where('email', '=', $email)
+            ->where('token', '=', $token)
+            ->get();
+
+        return !empty($result[0]);
+    }
+
+    private function deleteToken(string $email, string $token): bool
+    {
+        return DB::table('password_reset_tokens')
+            ->where('email', '=', $email)
+            ->where('token', '=', $token)
+            ->delete();
     }
 }
--- resources/views/mails/recover.blade.php
+++ resources/views/mails/recover.blade.php
@@ -1,5 +1,5 @@
 <h1>Recuperar Senha</h1>
 <p>Recuperar sua senha</p>
-<a href="https://localhost:8000/recover?token={{ $data['token'] }}">Recuperar</a>
+<a href="https://localhost:8000/recover?token={{ $data['token'] }}?user={{ $data['email'] }}">Recuperar</a>
 <p>{{ $data['message'] }}</p>
 
