2239f9a68cd18c4388eb1c53e113e6c5827db76f
wesley viana <wesley.vmartins.js@gmail.com>
2023-09-01 21:04:21 -0300

    Created recurrent and snackTime in preferences of workspace

--- database/migrations/2023_08_28_002311_create_workspaces_table.php
+++ database/migrations/2023_08_28_002311_create_workspaces_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('workspaces', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
+            $table->boolean('recurrent')->nullable();
+            $table->time('snack_time')->nullable();
 
             $table->foreignId('user_id')->constrained();
 
