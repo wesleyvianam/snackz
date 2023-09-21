aad652e63987786484ce814e3fa63da9f121bb86
wesley <wesley.vmartins.js@gmail.com>
2023-09-21 18:00:37 -0300

    Created tabs of configurations

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,3 +1,10 @@
+<style>
+    li {
+        cursor: pointer;
+        padding: 8px 16px;
+    }
+</style>
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -7,21 +14,27 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
+            <div x-data="{ activeTab: 'tab1' }">
+                <ul class="flex dark:text-white">
+                    <li @click="activeTab = 'tab1'" :class="{ 'bg-blue-500': activeTab === 'tab1' }">Tab 1</li>
+                    <li @click="activeTab = 'tab2'" :class="{ 'bg-blue-500': activeTab === 'tab2' }">Tab 2</li>
+                    <li @click="activeTab = 'tab3'" :class="{ 'bg-blue-500': activeTab === 'tab3' }">Tab 3</li>
+                </ul>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
+                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                    <div class="max-w-xl">
+                        <div x-show="activeTab === 'tab1'">
+                            @include('profile.partials.update-profile-information-form')
+                        </div>
+                        <div x-show="activeTab === 'tab2'">
+                            @include('profile.partials.update-password-form')
+                        </div>
+                        <div x-show="activeTab === 'tab3'">
+                            @include('profile.partials.delete-user-form')
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
