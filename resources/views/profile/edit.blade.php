2e2e1b8e666b3883f0328615247c8fa76a4d442a
wesley <wesley.vmartins.js@gmail.com>
2023-09-22 12:47:19 -0300

    updated layout of settings

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -14,26 +14,32 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div x-data="{ activeTab: 'members' }" class="flex justify-between">
+                <div class="w-1/4 border rounded me-5">
+                    <ul class="dark:text-white h-full">
+                        <li @click="activeTab = 'members'" :class="{ 'bg-gray-800 dark:bg-gray-200 dark:text-black': activeTab === 'members' }">Members</li>
+                        <li @click="activeTab = 'categories'" :class="{ 'bg-gray-800 dark:bg-gray-200 dark:text-black': activeTab === 'categories' }">Categories</li>
+                        <li @click="activeTab = 'snacks'" :class="{ 'bg-gray-800 dark:bg-gray-200 dark:text-black': activeTab === 'snacks' }">Snacks</li>
+                        <li @click="activeTab = 'profile'" :class="{ 'bg-gray-800 dark:bg-gray-200 dark:text-black': activeTab === 'profile' }">Profile</li>
+                        <li @click="activeTab = 'password'" :class="{ 'bg-gray-800 dark:bg-gray-200 dark:text-black': activeTab === 'password' }">Password</li>
+                        <li @click="activeTab = 'account'" :class="{ 'bg-gray-800 dark:bg-gray-200 dark:text-black': activeTab === 'account' }">Account</li>
+                    </ul>
+                </div>
 
-            <div x-data="{ activeTab: 'tab1' }">
-                <ul class="flex dark:text-white">
-                    <li @click="activeTab = 'tab1'" :class="{ 'bg-blue-500': activeTab === 'tab1' }">Tab 1</li>
-                    <li @click="activeTab = 'tab2'" :class="{ 'bg-blue-500': activeTab === 'tab2' }">Tab 2</li>
-                    <li @click="activeTab = 'tab3'" :class="{ 'bg-blue-500': activeTab === 'tab3' }">Tab 3</li>
-                </ul>
-
-                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                    <div class="max-w-xl">
-                        <div x-show="activeTab === 'tab1'">
+                <div class="w-3/4 p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg flex justify-center">
+                    <div class="max-w-">
+                        <div x-show="activeTab === 'members'">
+                            @include('profile.partials.members-management')
+                        </div>
+                        <div x-show="activeTab === 'profile'">
                             @include('profile.partials.update-profile-information-form')
                         </div>
-                        <div x-show="activeTab === 'tab2'">
+                        <div x-show="activeTab === 'password'">
                             @include('profile.partials.update-password-form')
                         </div>
-                        <div x-show="activeTab === 'tab3'">
+                        <div x-show="activeTab === 'account'">
                             @include('profile.partials.delete-user-form')
                         </div>
-
                     </div>
                 </div>
             </div>
