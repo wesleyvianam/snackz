16bdcb5bad37acf22b2a16039e9f2172c4181bec
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-09-24 10:21:17 -0300

    create list of members

--- database/migrations/2023_08_28_002332_create_members_table.php
+++ database/migrations/2023_08_28_002332_create_members_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
 
             $table->foreignId('workspace_id')->constrained();
 
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
 
             $table->timestamps();
         });
