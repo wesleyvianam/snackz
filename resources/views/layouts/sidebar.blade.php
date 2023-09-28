eea02bf52a373332898db66f8a45a859bfb4a836
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-09-28 06:57:39 -0300

    Created rule of show options for super user

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,24 +1,22 @@
-<div class="rounded-3xl w-30 h-56 bg-grey-100 dark:bg-gray-800 p-4 dark:text-white">
+<div class="rounded-3xl bg-grey-100 dark:bg-gray-800 p-4 dark:text-white">
     <ul class="w-full">
-        <li class="bg-gray-700 p-3 rounded-full px-8">
-            <i class="bi bi-house-fill pe-4"></i><a href="#">
-                Orders
-            </a>
+        <li class="p-3 rounded-full px-8">
+            <i class="bi bi-house-fill pe-4"></i>
+            <a href="/home">Home</a>
         </li>
-        <li class="p-3 rounded-full px-8 hover:bg-gray-700">
-            <i class="bi bi-people-fill pe-4"></i><a href="#">
-                Members
-            </a>
-        </li>
-        <li class="p-3 rounded-full px-8 hover:bg-gray-700">
-            <i class="bi bi-list-ul pe-4"></i><a href="#">
-                Categories
-            </a>
-        </li>
-        <li class="p-3 rounded-full px-8 hover:bg-gray-700">
-            <i class="bi bi-cup-hot-fill pe-4"></i><a href="#">
-                Snacks
-            </a>
-        </li>
+        @if (auth()->user()->super == 1)
+            <li class="bg-gray-700 p-3 rounded-full px-8 hover:bg-gray-700">
+                <i class="bi bi-people-fill pe-4"></i>
+                <a href="/members">Members</a>
+            </li>
+            <li class="p-3 rounded-full px-8 hover:bg-gray-700">
+                <i class="bi bi-list-ul pe-4"></i>
+                <a href="/categories">Categories</a>
+            </li>
+            <li class="p-3 rounded-full px-8 hover:bg-gray-700">
+                <i class="bi bi-cup-hot-fill pe-4"></i>
+                <a href="/snacks">Snacks</a>
+            </li>
+        @endif
     </ul>
 </div>
