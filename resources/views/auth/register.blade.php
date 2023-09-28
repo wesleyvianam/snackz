2403f0cd4a6053fe0008b079cd8bc9ba950c94d6
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-09-28 06:27:38 -0300

    updated button

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -62,9 +62,9 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <x-primary-button class="mt-4">
+        <x-primary-button-full class="mt-4">
             {{ __('Register') }}
-        </x-primary-button>
+        </x-primary-button-full>
 
         <div class="flex items-center justify-end mt-3">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
