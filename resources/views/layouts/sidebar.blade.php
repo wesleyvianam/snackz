ecc6c1c5cb814bff360f2b76022504ff707f759c
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-09-28 06:22:20 -0300

    create sidebar

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,24 @@
+<div class="rounded-3xl w-30 h-56 bg-grey-100 dark:bg-gray-800 p-4 dark:text-white">
+    <ul class="w-full">
+        <li class="bg-gray-700 p-3 rounded-full px-8">
+            <i class="bi bi-house-fill pe-4"></i><a href="#">
+                Orders
+            </a>
+        </li>
+        <li class="p-3 rounded-full px-8 hover:bg-gray-700">
+            <i class="bi bi-people-fill pe-4"></i><a href="#">
+                Members
+            </a>
+        </li>
+        <li class="p-3 rounded-full px-8 hover:bg-gray-700">
+            <i class="bi bi-list-ul pe-4"></i><a href="#">
+                Categories
+            </a>
+        </li>
+        <li class="p-3 rounded-full px-8 hover:bg-gray-700">
+            <i class="bi bi-cup-hot-fill pe-4"></i><a href="#">
+                Snacks
+            </a>
+        </li>
+    </ul>
+</div>
