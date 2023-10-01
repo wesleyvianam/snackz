de5018c29da19b360e51ab06696faac03506cfca
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-10-01 17:00:34 -0300

    Refactor function getWorkspaceID

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,7 +11,7 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 
-    public function getWorkspace()
+    protected function getWorkspaceId()
     {
         return Auth::user()->workspace_id;
     }
