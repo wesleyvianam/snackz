66d5240e0052d664b42616b1c2ac5b234280a50b
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-10-01 18:53:41 -0300

    refactor index snack with abstract list

--- resources/views/snacks/index.blade.php
+++ resources/views/snacks/index.blade.php
@@ -7,24 +7,7 @@
         @include('snacks.partials.modal-create', $formatCategories)
     </div>
 
-    <ul class="rounded dark:border-white bg-gray-700">
-        @if (!empty($snacks))
-            @foreach($snacks as $snack)
-                <li class="border-b p-2 flex justify-between items-center">
-                    <p>{{ $snack->name }}</p>
-
-                    <form action="{{ route('snacks.destroy', $snack->id) }}" method="post">
-                        @csrf
-                        @method('DELETE')
-
-                        <button class="rounded py-1 px-2 bg-red-500">Deletar</button>
-                    </form>
-                </li>
-            @endforeach
-        @else
-            <li class="p-2 flex justify-between items-center">
-                Nenhum lanche cadastrado
-            </li>
-        @endif
-    </ul>
+    <x-list route="snacks"
+            :data="$snacks"
+            emptyMessage="Tralala" />
 </x-app-layout>
