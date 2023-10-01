ad121e906f9191277d8702f382f63fe4362c3166
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-10-01 17:00:59 -0300

    Created logic of active tab

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,19 +1,19 @@
 <div class="rounded-3xl bg-grey-100 dark:bg-gray-800 p-4 dark:text-white">
     <ul class="w-full">
-        <li class="p-3 rounded-full px-8">
+        <li class="p-2 my-1 rounded-full px-8 hover:bg-gray-700 @if(Request::is('home')) bg-gray-700 @endif">
             <i class="bi bi-house-fill pe-4"></i>
             <a href="/home">Home</a>
         </li>
         @if (auth()->user()->super == 1)
-            <li class="bg-gray-700 p-3 rounded-full px-8 hover:bg-gray-700">
+            <li class="p-2 my-1 rounded-full px-8 hover:bg-gray-700 @if(Request::is('members')) bg-gray-700 @endif">
                 <i class="bi bi-people-fill pe-4"></i>
                 <a href="/members">Members</a>
             </li>
-            <li class="p-3 rounded-full px-8 hover:bg-gray-700">
+            <li class="p-2 my-1 rounded-full px-8 hover:bg-gray-700 @if(Request::is('categories')) bg-gray-700 @endif">
                 <i class="bi bi-list-ul pe-4"></i>
                 <a href="/categories">Categories</a>
             </li>
-            <li class="p-3 rounded-full px-8 hover:bg-gray-700">
+            <li class="p-2 my-1 rounded-full px-8 hover:bg-gray-700 @if(Request::is('snacks')) bg-gray-700 @endif">
                 <i class="bi bi-cup-hot-fill pe-4"></i>
                 <a href="/snacks">Snacks</a>
             </li>
