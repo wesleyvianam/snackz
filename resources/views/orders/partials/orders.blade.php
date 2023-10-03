4a7139f706fdf8513db297d0d24f7f276cf8af5d
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-10-03 06:14:13 -0300

    page resume order

---
+++ resources/views/orders/partials/orders.blade.php
@@ -0,0 +1,9 @@
+{{--@foreach($orders as $order)--}}
+{{--    <h1 class="text-3xl font-bold">--}}
+{{--        {{ $order->member }}--}}
+{{--    </h1>--}}
+
+{{--    @foreach($item->snacks as $snack)--}}
+{{--        <p>{{ $snack->name }}</p>--}}
+{{--    @endforeach--}}
+{{--@endforeach--}}
