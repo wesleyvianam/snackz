29f90266154d3914653e8cdba78e32c0fa047737
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-10-04 06:37:13 -0300

    Conclude first show of screen to make order

--- resources/views/orders/partials/orders.blade.php
+++ resources/views/orders/partials/orders.blade.php
@@ -1,9 +1,27 @@
-{{--@foreach($orders as $order)--}}
-{{--    <h1 class="text-3xl font-bold">--}}
-{{--        {{ $order->member }}--}}
-{{--    </h1>--}}
+<div class="bg-gray-700 rounded-xl p-3">
+    <h1 class=" text-center text-3xl font-bold mb-3">
+        Order
+    </h1>
+{{--        <h1 class="text-xl font-bold mb-2">--}}
+{{--            {{ $order["title"] }}--}}
+{{--        </h1>--}}
 
-{{--    @foreach($item->snacks as $snack)--}}
-{{--        <p>{{ $snack->name }}</p>--}}
-{{--    @endforeach--}}
-{{--@endforeach--}}
+    <table class="text-white w-full mb-4 rounded-b-lg">
+        <thead>
+            <tr class="bg-gray-900 rounded-t-lg">
+                <th class="rounded-s-lg text-start py-2 px-3 w-1/2">Item</th>
+                <th class="rounded-e-lg text-start py-2 px-3 w-1/2">Qtd</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($ordersResume as $order)
+                @foreach($order['snack'] as $snack)
+                    <tr>
+                        <td class="w-1/2 py-1 px-3">{{ $snack['title'] }}</td>
+                        <td class="w-1/2 py-1 px-3">{{ $snack['qtd'] }}</td>
+                    </tr>
+                @endforeach
+            @endforeach
+        </tbody>
+    </table>
+</div>
