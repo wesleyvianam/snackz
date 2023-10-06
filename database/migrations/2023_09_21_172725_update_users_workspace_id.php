e139e8ce84a631a26cdec8afb3be01e19c4ac962
wesley <wesley.vmartins.js@gmail.com>
2023-10-06 17:29:48 -0300

    Updated migration user for new architecture

--- database/migrations/2023_09_21_172725_update_users_workspace_id.php
+++ database/migrations/2023_09_21_172725_update_users_workspace_id.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('workspace_id')->nullable()->constrained();
+            $table->foreignId('workspace_id')->constrained();
         });
     }
 
