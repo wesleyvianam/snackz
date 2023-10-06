0692c14ed69aed203e645b94678d22c56bbd80ba
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-10-06 20:45:57 -0300

    updated migration workspace for to use tinyInteger  and not Integer for boolean

--- database/migrations/2023_08_28_002311_create_workspaces_table.php
+++ database/migrations/2023_08_28_002311_create_workspaces_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('workspaces', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
-            $table->integer('config');
+            $table->tinyInteger('config');
 
             $table->timestamps();
         });
