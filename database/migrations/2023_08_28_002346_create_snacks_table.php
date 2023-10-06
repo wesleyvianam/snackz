b2c29d34be12630f0cf4c03a027dc6cb92889035
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-10-06 20:45:28 -0300

    updated migration snack for to use tinyInteger  and not Integer for boolean

--- database/migrations/2023_08_28_002346_create_snacks_table.php
+++ database/migrations/2023_08_28_002346_create_snacks_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('snacks', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->tinyInteger('is_recurrent');
 
             $table->foreignId('category_id')->constrained();
 
