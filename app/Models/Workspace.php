043437d2220f70fbfd8054ba4c4d0d8993e9eb02
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-10-06 20:55:16 -0300

    refactored fillable and created relation with workspace_settings

--- app/Models/Workspace.php
+++ app/Models/Workspace.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Workspace extends Model
 {
@@ -13,9 +14,6 @@ class Workspace extends Model
 
     protected $fillable = [
         'name',
-        'recurrent',
-        'snack_time',
-        'user_id',
     ];
 
     public function members(): HasMany
@@ -36,5 +34,10 @@ class Workspace extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function settings(): HasOne
+    {
+        return $this->hasOne(WorkspaceSetting::class);
     }
 }
