635678321985717b86c79f6d05595ae2e578cf5b
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-10-07 08:53:04 -0300

    Updated username for name

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
     protected $fillable = [
         'email',
         'password',
-        'username',
+        'name',
         'config',
         'workspace_id',
         'super'
