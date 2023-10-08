1f0c8097caf0f56a4719bfda65e409361aacd5b0
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-10-08 17:15:38 -0300

    updated query builder for new architecture member like user

--- app/Http/Controllers/Order/OrderController.php
+++ app/Http/Controllers/Order/OrderController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers\Order;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
-use App\Models\Member;
 use App\Models\Order;
-use App\Models\SnackTemplate;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -20,13 +18,13 @@ class OrderController extends Controller
         $categories = Category::where('workspace_id', $this->getWorkspaceId())->with('snacks')->get();
 
         $orders = DB::table('orders as o')
-            ->join('members as m', 'm.id', '=','o.member_id')
+            ->join('users as m', 'm.id', '=','o.user_id')
             ->join('snacks as s', 's.id', '=','o.snack_id')
             ->join('categories as c', 'c.id', '=','s.category_id')
             ->where('o.workspace_id', '=', $this->getWorkspaceId())
             ->selectRaw("
-                m.name as member,
-                m.id as member_id,
+                m.name as user,
+                m.id as user_id,
                 s.name as snack,
                 s.id as snack_id,
                 c.title as category,
@@ -36,8 +34,8 @@ class OrderController extends Controller
 
         $ordersDetails = [];
         foreach ($orders as $order) {
-            $ordersDetails[$order->member_id]['name'] = $order->member;
-            $ordersDetails[$order->member_id]['snack'][] = $order->snack;
+            $ordersDetails[$order->user_id]['name'] = $order->user;
+            $ordersDetails[$order->user_id]['snack'][] = $order->snack;
         }
 
         $ordersResume = [];
@@ -60,31 +58,28 @@ class OrderController extends Controller
 
     public function store(Request $request): RedirectResponse
     {
-        $member = Member::where('user_id', Auth::user()->id)->get();
-        $workspaceId = $this->getWorkspaceId();
+        $user = Auth::user();
+        $recurrent = 0;
 
         $values = [];
         foreach ($request->request as $key => $snack) {
             if ($key != '_token' && $key != 'recurrent') {
                 $values[] = $snack;
             }
+
+            if ($key == 'recurrent') {
+                $recurrent = $snack ?: 0;
+            }
         }
 
-        DB::transaction(function () use ($request, $member, $values, $workspaceId) {
+        DB::transaction(function () use ($recurrent, $user, $values) {
             foreach ($values as $snack) {
                 Order::create([
                     'snack_id' => $snack,
-                    'member_id' => $member[0]->id,
-                    'workspace_id' => $workspaceId
+                    'user_id' => $user->id,
+                    'workspace_id' => $user->workspace_id,
+                    'recurrent' => $recurrent
                 ]);
-
-                if ($request->recurrent) {
-                    SnackTemplate::create([
-                        'snack_id' => $snack,
-                        'member_id' => $member[0]->id,
-                        'workspace_id' => $workspaceId
-                    ]);
-                }
             }
         });
 
