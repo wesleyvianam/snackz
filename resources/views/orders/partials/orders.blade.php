efa9295a9080f5a3186d60fc42b932de17cf45f0
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-10-07 21:29:16 -0300

    created exception for when don't have orders

--- resources/views/orders/partials/orders.blade.php
+++ resources/views/orders/partials/orders.blade.php
@@ -1,24 +1,26 @@
 <div class="bg-gray-100 dark:bg-gray-700 rounded-xl p-3">
-    <h1 class="text-center text-3xl font-bold mb-3">
-        Order
-    </h1>
-
-    <table class="dark:text-white w-full mb-4 rounded-b-lg">
-        <thead>
-            <tr class="bg-gray-300 dark:bg-gray-900 rounded-t-lg">
-                <th class="rounded-s-lg text-start py-2 px-3 w-1/2">Item</th>
-                <th class="rounded-e-lg text-start py-2 px-3 w-1/2">Qtd</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($ordersResume as $order)
-                @foreach($order['snack'] as $snack)
-                    <tr>
-                        <td class="w-1/2 py-1 px-3">{{ $snack['title'] }}</td>
-                        <td class="w-1/2 py-1 px-3">{{ $snack['qtd'] }}</td>
-                    </tr>
+    @if(!empty($ordersResume))
+        <table class="dark:text-white w-full mb-4 rounded-b-lg">
+            <thead>
+                <tr class="bg-gray-300 dark:bg-gray-900 rounded-t-lg">
+                    <th class="rounded-s-lg text-start py-2 px-3 w-1/2">Item</th>
+                    <th class="rounded-e-lg text-start py-2 px-3 w-1/2">Qtd</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($ordersResume as $order)
+                    @foreach($order['snack'] as $snack)
+                        <tr>
+                            <td class="w-1/2 py-1 px-3">{{ $snack['title'] }}</td>
+                            <td class="w-1/2 py-1 px-3">{{ $snack['qtd'] }}</td>
+                        </tr>
+                    @endforeach
                 @endforeach
-            @endforeach
-        </tbody>
-    </table>
+            </tbody>
+        </table>
+    @else
+        <div>
+            <i>Nenhum pedido realizado.</i>
+        </div>
+    @endif
 </div>
