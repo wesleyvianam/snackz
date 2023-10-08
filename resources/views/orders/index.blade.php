28e4972788183801b4bfda8fcd054a3144b5dfc8
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-10-07 21:32:13 -0300

    Updated styles of index ordes

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -7,10 +7,24 @@
 
 <x-app-layout>
         <div x-data="{ activeTab: 'orders' }" class="">
-            <ul class="flex justify-center border-b">
-                <li @click="activeTab = 'orders'" :class="{ 'rounded-t-lg text-white bg-gray-800 dark:bg-gray-200 dark:text-black': activeTab === 'orders' }">Orders</li>
-                <li @click="activeTab = 'resume'" :class="{ 'rounded-t-lg text-white bg-gray-800 dark:bg-gray-200 dark:text-black': activeTab === 'resume' }">Details</li>
-                <li @click="activeTab = 'new'" :class="{ 'rounded-t-lg text-white bg-gray-800 dark:bg-gray-200 dark:text-black': activeTab === 'new' }">New</li>
+            <ul class="flex justify-center border-b text-center">
+                <li class="w-1/3"
+                    @click="activeTab = 'orders'"
+                    :class="{ 'rounded-t-lg text-white bg-gray-800 dark:bg-gray-200 dark:text-black': activeTab === 'orders' }">
+                    Pedidos
+                </li>
+
+                <li class="w-1/3"
+                    @click="activeTab = 'resume'"
+                    :class="{ 'rounded-t-lg text-white bg-gray-800 dark:bg-gray-200 dark:text-black': activeTab === 'resume' }">
+                    Pedidos detalhados
+                </li>
+
+                <li class="w-1/3"
+                    @click="activeTab = 'new'"
+                    :class="{ 'rounded-t-lg text-white bg-gray-800 dark:bg-gray-200 dark:text-black': activeTab === 'new' }">
+                    Novo pedido
+                </li>
             </ul>
 
             <div class="py-4">
