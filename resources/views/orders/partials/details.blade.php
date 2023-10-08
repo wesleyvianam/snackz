b1b19a8d87b2ea0e59aafbe2cddbe756757059e9
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-10-07 21:29:09 -0300

    created exception for when don't have orders

--- resources/views/orders/partials/details.blade.php
+++ resources/views/orders/partials/details.blade.php
@@ -1,24 +1,27 @@
-<h1 class="text-xl font-bold mb-4">
-    Orders
-</h1>
-<div class="grid grid-cols-3 gap-4">
-    @foreach($ordersDetails as $order)
-{{--        @dd($order)--}}
-        <div class="border rounded-lg bg-gray-100 dark:bg-gray-600">
-            <h2 class="text-center py-2">{{ $order['name'] }}</h2>
-            <hr>
-            <ul class="w-full dark:text-white mx-4">
-                <li class="flex items-center justify-between">
-                    <p class="text-start text-bold">Item</p>
-                    <p class="pr-10 text-bold">Qtd</p>
-                </li>
-                @foreach($order['snack'] as $snack)
+@if (!empty($ordersDetails))
+    <div class="grid grid-cols-3 gap-4">
+        @foreach($ordersDetails as $order)
+            <div class="border rounded-lg bg-gray-100 dark:bg-gray-600">
+                <h2 class="text-center py-2">{{ $order['name'] }}</h2>
+                <hr>
+                <ul class="w-full dark:text-white mx-4">
                     <li class="flex items-center justify-between">
-                        <p class="">{{ $snack }}</p>
-                        <p class="pr-10">1</p>
+                        <p class="text-start text-bold">Item</p>
+                        <p class="prn-10 text-bold">Qtd</p>
                     </li>
-                @endforeach
-            </ul>
-        </div>
-    @endforeach
-</div>
+                    @foreach($order['snack'] as $snack)
+                        <li class="flex items-center justify-between">
+                            <p class="">{{ $snack }}</p>
+                            <p class="pr-10">1</p>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        @endforeach
+    </div>
+@else
+    <div class="bg-gray-100 dark:bg-gray-700 rounded-xl p-3">
+        <i>Nenhum pedido realizado</i>
+    </div>
+@endif
+
