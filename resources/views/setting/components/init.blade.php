e10ae8db70aaee2edd211d065f84a2e437b026f2
wesley <wesley.vmartins.js@gmail.com>
2023-10-09 09:51:24 -0300

    create init of setting workspace

--- resources/views/setting/components/init.blade.php
+++ resources/views/setting/components/init.blade.php
@@ -1,2 +1,10 @@
+<div class="text-center">
+    <h1 class="text-3xl font-bold pb-5">
+        Iniciar configuração de workspace
+    </h1>
 
+    <x-primary-button @click="setting = 'company'">
+        {{ __('Iniciar') }}
+    </x-primary-button>
+</div>
 
