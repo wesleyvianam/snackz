12b2b5a067ae8ccd84833b730804920d0e56e734
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-10-10 07:11:03 -0300

    updated paddings

--- resources/views/setting/components/init.blade.php
+++ resources/views/setting/components/init.blade.php
@@ -1,10 +1,9 @@
-<div class="text-center">
-    <h1 class="text-3xl font-bold pb-5">
+<div class="text-center pb-7">
+    <h1 class="text-3xl font-bold mt-10 pb-6">
         Iniciar configuração de workspace
     </h1>
 
-    <x-primary-button @click="setting = 'company'">
+    <x-primary-button @click="setting = 'company'; percent += 1">
         {{ __('Iniciar') }}
     </x-primary-button>
 </div>
-
