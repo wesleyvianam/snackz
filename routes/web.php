aedd0ed01ff2f67c6b2c68f58d96ddf8441e1f6f
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-10-28 07:21:12 -0300

    Updated routes

--- routes/web.php
+++ routes/web.php
@@ -31,10 +31,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // Config Workspace
-    Route::get('/setting', [SettingsController::class, 'index'])->name('setting.index');
-    Route::post('/company', [CompanyController::class, 'store'])->name('company.store');
-
     Route::get('/members', [MemberController::class, 'index'])->name('members.index');
     Route::post('/members', [MemberController::class, 'store'])->name('members.store');
     Route::delete('/members/{user}', [MemberController::class, 'destroy'])->name('members.destroy');
