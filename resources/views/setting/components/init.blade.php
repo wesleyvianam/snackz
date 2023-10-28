8b7698139c1597b0e0ee7c06de565c9dd1da0d4a
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-10-28 07:20:56 -0300

    removed archives

--- resources/views/setting/components/init.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="text-center pb-7">
-    <h1 class="text-3xl font-bold mt-10 pb-6">
-        Iniciar configuração de workspace
-    </h1>
-
-    <x-primary-button @click="setting = 'company'; percent += 1">
-        {{ __('Iniciar') }}
-    </x-primary-button>
-</div>
