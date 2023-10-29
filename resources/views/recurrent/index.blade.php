fdbfea57048aab0b91f11c74fe5202e607d72939
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-10-29 07:44:21 -0300

    added categorias

---
+++ resources/views/recurrent/index.blade.php
@@ -0,0 +1,35 @@
+<x-app-layout>
+    <div class="flex justify-between items-center pb-3">
+        <h1 class="text-2xl font-bold">Pedidos Recorrentes</h1>
+    </div>
+
+    <ul class="rounded-lg border border-red-100 dark:border-red-200 dark:bg-gray-700">
+        @if (!empty($orders))
+            @foreach($orders as $order)
+                <li class="p-3">
+                    {{ $order['title'] }}
+
+                    <hr class="my-2">
+                    @foreach($order['orders'] as $snack)
+                        <span class="p-2 px-4 rounded-lg flex items-center justify-between hover:bg-red-50">
+                            {{ $snack['title'] }}
+
+                            <form action="{{ route('recurrent.update', $snack['id']) }}" method="post">
+                                @csrf
+                                @method('PUT')
+
+                                <button class="rounded py-1 px-2">
+                                    <i class="bi bi-trash text-red-500"></i>
+                                </button>
+                            </form>
+                        </span>
+                    @endforeach
+                </li>
+            @endforeach
+        @else
+            <div class="bg-red-50 text-gray-800 border border-red-100 dark:text-gray-200 dark:bg-gray-700 rounded-xl p-3">
+                <i>Nenhum item recorrente.</i>
+            </div>
+        @endif
+    </ul>
+</x-app-layout>
