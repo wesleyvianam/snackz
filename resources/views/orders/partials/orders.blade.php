9bd95b78bc7c69565720e60a4a6abf36fcb8c93f
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-10-30 05:44:32 -0300

    improvements layout of orders list

--- resources/views/orders/partials/orders.blade.php
+++ resources/views/orders/partials/orders.blade.php
@@ -1,18 +1,18 @@
 @if(!empty($ordersResume))
-    <div class="bg-red-50 dark:bg-gray-700 rounded-xl p-3">
-        <table class="dark:text-white w-full mb-4 rounded-b-lg">
+    <div class="border border-red-50 dark:bg-gray-700 dark:border-gray-900 rounded-xl">
+        <table class="dark:text-white w-full mb-2 rounded-b-lg">
             <thead>
-                <tr class="bg-gray-300 dark:bg-gray-900 rounded-t-lg">
-                    <th class="rounded-s-lg text-start py-2 px-3 w-1/2">Item</th>
-                    <th class="rounded-e-lg text-start py-2 px-3 w-1/2">Qtd</th>
+                <tr class="bg-red-50 dark:bg-gray-900 rounded-t-lg">
+                    <th class="rounded-tl-xl text-start py-2 px-3 w-1/2">Item</th>
+                    <th class="rounded-tr-xl text-start py-2 px-3 w-1/2">Quantidade</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach($ordersResume as $order)
                     @foreach($order['snack'] as $snack)
-                        <tr>
-                            <td class="w-1/2 py-1 px-3">{{ $snack['title'] }}</td>
-                            <td class="w-1/2 py-1 px-3">{{ $snack['qtd'] }}</td>
+                        <tr class="border-b border-red-50 dark:border-gray-800">
+                            <td class="w-1/2 py-2 px-3">{{ $snack['title'] }}</td>
+                            <td class="w-1/2 py-2 px-3">{{ $snack['qtd'] }}</td>
                         </tr>
                     @endforeach
                 @endforeach
