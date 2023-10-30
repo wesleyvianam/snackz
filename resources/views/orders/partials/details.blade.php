c4b6125797b6c20f116d5a70c9b1bacb371c446f
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-10-30 07:22:16 -0300

    Improvements details screen

--- resources/views/orders/partials/details.blade.php
+++ resources/views/orders/partials/details.blade.php
@@ -1,14 +1,17 @@
 @if (!empty($ordersDetails))
     <div class="grid grid-cols-3 gap-4">
         @foreach($ordersDetails as $order)
-            <div class="border rounded-lg bg-red-50 dark:bg-gray-600">
-                <h2 class="text-center py-2">{{ $order['name'] }}</h2>
-                <hr>
-                <ul class="w-full dark:text-white mx-4">
-                    <li class="flex items-center justify-between">
-                        <p class="text-start text-bold">Item</p>
-                        <p class="prn-10 text-bold">Qtd</p>
+            <div class="border rounded-lg dark:bg-gray-600">
+                <h2 class="text-center py-2 border-b font-bold text-gray-700 dark:text-white">
+                    {{ $order['name'] }}
+                </h2>
+
+                <ul class="w-full px-4">
+                    <li class="flex items-center dark:text-white justify-between font-bold text-gray-700">
+                        <p>Item</p>
+                        <p>Quantidade</p>
                     </li>
+
                     @foreach($order['snack'] as $snack)
                         <li class="flex items-center justify-between">
                             <p class="">{{ $snack }}</p>
