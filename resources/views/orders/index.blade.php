e1e17ec32e6d06d932f2ef1b6fa1b93d24e77013
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-10-30 07:22:37 -0300

    Improvements index orders screen

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,32 +1,24 @@
-<style>
-    li {
-        cursor: pointer;
-        padding: 8px 16px;
-    }
-</style>
-
 <x-app-layout>
-        <div x-data="{ activeTab: 'orders' }" class="">
-            <ul class="flex justify-center text-red-500 border-b border-red-400 dark:text-white text-center">
-                <li class="w-1/3"
+        <div x-data="{ activeTab: 'orders' }">
+            <ul class="flex justify-center text-red-500 border-b border-red-400 dark:text-white text-center cursor-pointer">
+                <li class="w-1/3 py-2"
                     @click="activeTab = 'orders'"
-                    :class="{ 'rounded-t-lg text-white bg-red-500 dark:bg-red-400': activeTab === 'orders' }">
+                    :class="{ 'rounded-t-lg text-white bg-red-500': activeTab === 'orders' }">
                     Pedidos
                 </li>
 
-                <li class="w-1/3"
+                <li class="w-1/3 py-2"
                     @click="activeTab = 'resume'"
-                    :class="{ 'rounded-t-lg text-white bg-red-500 dark:bg-red-400': activeTab === 'resume' }">
+                    :class="{ 'rounded-t-lg text-white bg-red-500': activeTab === 'resume' }">
                     Pedidos detalhados
                 </li>
 
-                <li class="w-1/3"
+                <li class="w-1/3 py-2"
                     @click="activeTab = 'new'"
-                    :class="{ 'rounded-t-lg text-white bg-red-500 dark:bg-red-400': activeTab === 'new' }">
+                    :class="{ 'rounded-t-lg text-white bg-red-500': activeTab === 'new' }">
                     Novo pedido
                 </li>
             </ul>
-
             <div class="pt-4">
                 <div x-show="activeTab === 'orders'">
                     @include('orders.partials.orders')
