04080c4b7f816096243943957193b4a1521c77d4
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-11-02 11:05:37 -0300

    Update orders

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,9 +1,9 @@
 <x-app-layout>
-        <div x-data="{ activeTab: 'orders' }">
+        <div x-data="{ activeTab: 'order' }">
             <ul class="flex justify-center text-red-500 border-b border-red-400 dark:text-white text-center cursor-pointer">
                 <li class="w-1/3 py-2"
-                    @click="activeTab = 'orders'"
-                    :class="{ 'rounded-t-lg text-white bg-red-500': activeTab === 'orders' }">
+                    @click="activeTab = 'order'"
+                    :class="{ 'rounded-t-lg text-white bg-red-500': activeTab === 'order' }">
                     Pedidos
                 </li>
 
@@ -20,11 +20,11 @@
                 </li>
             </ul>
             <div class="pt-4">
-                <div x-show="activeTab === 'orders'">
-                    @include('orders.partials.orders')
+                <div x-show="activeTab === 'order'">
+                    @include('orders.partials.details')
                 </div>
                 <div x-show="activeTab === 'resume'">
-                    @include('orders.partials.details')
+                    @include('orders.partials.orders')
                 </div>
                 <div x-show="activeTab === 'new'">
                     @include('orders.partials.new')
