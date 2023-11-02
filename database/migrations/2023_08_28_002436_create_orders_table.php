f459cf0c8411f8f481b46b24c727fb119de26b96
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-11-02 19:56:16 -0300

    Added field quantity

--- database/migrations/2023_08_28_002436_create_orders_table.php
+++ database/migrations/2023_08_28_002436_create_orders_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->tinyInteger('recurrent');
+            $table->integer('quantity');
 
             $table->foreignId('snack_id')->constrained()->onDelete('cascade');
 
