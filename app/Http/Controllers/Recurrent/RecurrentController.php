8974fce052e17c31c8fd7d5bae92b094069561a1
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-11-03 16:22:03 -0300

    updated algorithm of get orders

--- app/Http/Controllers/Recurrent/RecurrentController.php
+++ app/Http/Controllers/Recurrent/RecurrentController.php
@@ -12,30 +12,11 @@ class RecurrentController extends Controller
 {
     public function index()
     {
-        $orders = DB::table('orders as o')
-            ->join('snacks as s', 's.id', '=','o.snack_id')
-            ->join('categories as c', 'c.id', '=','s.category_id')
-            ->where('o.user_id', '=', Auth::user()->id)
-            ->where('o.recurrent', '=', 1)
-            ->selectRaw("
-                s.name as snack,
-                o.id as order_id,
-                c.title as category,
-                c.id as category_id
-            ")
-            ->orderBy('s.category_id', 'asc')
-            ->get();
+        $orders = Auth::user()->super
+            ? $this->getAllOrdersRecurrent()
+            : $this->getOrdersRecurrentByUser();
 
-        $ordersByCategory = [];
-        foreach ($orders as $order) {
-            $ordersByCategory[$order->category_id]['title'] = $order->category;
-            $ordersByCategory[$order->category_id]['orders'][] = [
-                'id' => $order->order_id,
-                'title' => $order->snack,
-            ];
-        }
-
-        return view('recurrent.index')->with('orders', $ordersByCategory);
+        return view('recurrent.index')->with('orders', $orders);
     }
 
     public function update(Order $order)
@@ -45,5 +26,66 @@ class RecurrentController extends Controller
         ]);
 
         return to_route('recurrent.index');
+    }
+
+    private function getAllOrdersRecurrent()
+    {
+        $orders = DB::table('orders as o')
+            ->join('snacks as s', 's.id', '=','o.snack_id')
+            ->join('categories as c', 'c.id', '=','s.category_id')
+            ->join('users as u', 'u.id', '=','o.user_id')
+            ->where('o.recurrent', '=', 1)
+            ->selectRaw("
+                s.name as snack,
+                o.id as order_id,
+                c.title as category,
+                c.id as category_id,
+                u.id as user_id,
+                u.name as username,
+                o.quantity as quantity
+            ")
+            ->orderBy('s.category_id', 'asc')
+            ->get();
+
+        return $this->hydrateDate($orders);
+    }
+
+    private function getOrdersRecurrentByUser()
+    {
+        $orders = DB::table('orders as o')
+            ->join('snacks as s', 's.id', '=','o.snack_id')
+            ->join('categories as c', 'c.id', '=','s.category_id')
+            ->join('users as u', 'u.id', '=','o.user_id')
+            ->where('o.user_id', '=', Auth::user()->id)
+            ->where('o.recurrent', '=', 1)
+            ->selectRaw("
+                s.name as snack,
+                o.id as order_id,
+                c.title as category,
+                c.id as category_id,
+                u.id as user_id,
+                u.name as username,
+                o.quantity as quantity
+            ")
+            ->orderBy('s.category_id', 'asc')
+            ->get();
+
+        return $this->hydrateDate($orders);
+    }
+
+    private function hydrateDate($ordersRecurrent)
+    {
+        $ordersByUser = [];
+        foreach ($ordersRecurrent as $order) {
+            $ordersByUser[$order->user_id]['title'] = $order->username;
+            $ordersByUser[$order->user_id]['orders'][] = [
+                'id' => $order->order_id,
+                'title' => $order->snack,
+                'category' => $order->category,
+                'quantity' => $order->quantity,
+            ];
+        }
+
+        return $ordersByUser;
     }
 }
