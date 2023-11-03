63bc0ea81fd92626a4ae4ba07dab9d3f3b4b95ba
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-11-03 16:22:59 -0300

    updated list of orders

--- resources/views/recurrent/index.blade.php
+++ resources/views/recurrent/index.blade.php
@@ -3,34 +3,44 @@
         <h1 class="text-2xl font-bold">Pedidos Recorrentes</h1>
     </div>
 
-    <ul class="rounded-lg border border-red-100 dark:border-red-200 dark:bg-gray-700">
-        @if (!empty($orders))
-            @foreach($orders as $order)
-                <li class="p-3">
-                    <h2 class="border-b">
+    @if (!empty($orders))
+        <div class="grid {{ auth()->user()->super ? 'grid-cols-2' : 'grid-cols-1' }} gap-4">
+            @foreach($orders as $key => $order)
+                <div class="border rounded-lg dark:bg-gray-600">
+                    <h2 class="text-center py-2 border-b font-bold text-gray-700 dark:text-white">
                         {{ $order['title'] }}
                     </h2>
 
-                    @foreach($order['orders'] as $snack)
-                        <span class="p-2 px-4 flex items-center justify-between hover:bg-red-50">
-                            {{ $snack['title'] }}
+                    <ul class="w-full px-4">
+                        @foreach($order['orders'] as $snack)
+                            <li class="flex items-center justify-between py-1">
+                                <p class="w-2/5">{{ $snack['title'] }}</p>
 
-                            <form action="{{ route('recurrent.update', $snack['id']) }}" method="post">
-                                @csrf
-                                @method('PUT')
+                                <p class="w-1/5">{{ $snack['quantity'] }}</p>
 
-                                <button class="rounded py-1 px-2">
-                                    <i class="bi bi-trash text-red-500"></i>
-                                </button>
-                            </form>
-                        </span>
-                    @endforeach
-                </li>
+                                <div class="w-2/5">
+                                    <span class="bg-indigo-100 text-indigo-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-indigo-400 border border-indigo-400">
+                                        {{ $snack['category'] }}
+                                    </span>
+                                </div>
+
+                                <form action="{{ route('recurrent.update', $snack['id']) }}" method="post">
+                                    @csrf
+                                    @method('PUT')
+
+                                    <button class="rounded py-1 px-2">
+                                        <i class="bi bi-trash text-red-500"></i>
+                                    </button>
+                                </form>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
             @endforeach
-        @else
-            <div class="bg-red-50 text-gray-800 border border-red-100 dark:text-gray-200 dark:bg-gray-700 rounded-xl p-3">
-                <i>Nenhum item recorrente.</i>
-            </div>
-        @endif
-    </ul>
+        </div>
+    @else
+        <div class="bg-red-50 text-gray-800 border border-red-100 dark:bg-gray-700 dark:text-gray-200 rounded-xl p-3">
+            <i>Nenhum pedido recorrente.</i>
+        </div>
+    @endif
 </x-app-layout>
